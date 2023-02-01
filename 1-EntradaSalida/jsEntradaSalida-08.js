b4718872ff2ee9f03c05dfbd8e08d3a91551185d
DMateo <devitamateo16@gmail.com>
2023-02-01 12:32:42 -0300

    Ejercicio 8 Devita Mateo

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,24 @@
-/*
+/* 
+Devita Mateo
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	let numeroUno; 
+	let numeroDos; 
+	let numUnoTexto; 
+	let numDosTexto; 
+	let mensaje; 
+
+	numUnoTexto=document.getElementById("txtIdNumeroDividendo").value; 
+	numDosTexto=document.getElementById("txtIdNumeroDivisor").value;  
+
+	numeroUno=parseInt(numUnoTexto);  
+	numeroDos=parseInt(numDosTexto);  
+
+	mensaje="El resto es " + (numeroUno % numeroDos); 
+
+	alert(mensaje); 
 }
