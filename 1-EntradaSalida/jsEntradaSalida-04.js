1670e7f3b5610b4d62b984d7c50c95d988a1096c
DMateo <devitamateo16@gmail.com>
2023-02-01 09:50:42 -0300

    Ejercicio 4 Devita Mateo

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,12 @@
 /* 
+Devita Mateo
 Debemos lograr tomar un dato por 'PROMPT' 
-y lo muestro por 'ID' al presionar el botón 'mostrar'
+y lo muestro por 'ID.value' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
+{   
+	let datoIngresado; 
+    datoIngresado= prompt("Ingresar Dato");  
+	document.getElementById("txtIdNombre").value = datoIngresado; 
 }
 
