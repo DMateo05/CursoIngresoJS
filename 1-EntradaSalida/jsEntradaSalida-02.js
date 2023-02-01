bf60703a4ff7ae14809de94879ca7bac2258f9df
DMateo <devitamateo16@gmail.com>
2023-02-01 14:34:59 -0300

    Ejercicio 2 Devita Mateo

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -4,7 +4,6 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/ 
 function mostrar()
 { 
-	
 	let nombrePersona; 
 	let apellidoPersona;  
 	nombrePersona = prompt("¿Cuál es tu nombre?"); 
