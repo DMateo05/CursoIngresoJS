789074156ad96d81ecd988038a88d5f91dcb90ae
DMateo <devitamateo16@gmail.com>
2023-02-02 07:19:08 -0300

    Ejercicio 5BIS Devita Mateo

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -4,6 +4,7 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
+	// Ejercicio 5
 	let nombrePersona; 
 	let edadPersona; 
 	let mensaje; 
@@ -11,5 +12,19 @@ function mostrar()
 	edadPersona = document.getElementById("txtIdEdad").value;  
 	mensaje = "Usted se llama: " + nombrePersona + " y tiene " + edadPersona;
 	alert(mensaje);  
+	// Fin Ejercicio 5 
+
+	// Inicio Ejercicio 5BIS 
+	//let nombrePersona;  
+	//let edadPersona; 
+	//let apellidoPersona; 
+	//let mensaje; 
+
+	//nombrePersona = document.getElementById("txtIdNombre").value; 
+	//edadPersona = document.getElementById("txtIdEdad").value; 
+	//apellidoPersona=prompt("Ingrese su Apellido"); 
+	//mensaje=apellidoPersona + ", usted se llama " + nombrePersona + " y tiene " + edadPersona + " años"; 
+	//alert(mensaje); 
+	// Fin Ejercicio 5BIS
 }
 
