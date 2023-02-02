06e3045b7001ea894d97fb981aaf3ee36b4e2c5e
DMateo <devitamateo16@gmail.com>
2023-02-02 06:52:52 -0300

    Ejercicio 3BIS Devita Mateo

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,12 +4,30 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+    // Ejercicio 3 
 	let nombreIngresado; 
-    let mensaje;
+    let mensaje; 
+     
     //nombreIngresado=txtIdNombre.value; Solo funciona en Google
     nombreIngresado = document.getElementById("txtIdNombre").value;
     mensaje = "Vos te llamas: " + nombreIngresado; 
-    alert(mensaje);  
+    alert(mensaje);   
+    // Fin Ejercicio 3 
+
+    // Ejercicio 3Bis 
+     //let precioIngresado; 
+     //let aumento; 
+     //let porcentaje = 30;  
+     //let aumentoDos; 
+     //let descripcionProducto; 
+     
+     //precioIngresado=parseInt(document.getElementById("txtIdNombre").value); 
+     //descripcionProducto=prompt("Ingrese Descripcion del Producto"); 
+     
+     //aumento=(precioIngresado*porcentaje/100); 
+     //aumentoDos=(precioIngresado+aumento); 
+     //alert(descripcionProducto+aumentoDos); 
+    // Fin Ejercicio 3Bis
 }
   
 
