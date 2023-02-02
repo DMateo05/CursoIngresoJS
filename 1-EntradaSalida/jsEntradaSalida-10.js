53f1620ba9d6aeb817d4e4de3d6f641c208f35ed
DMateo <devitamateo16@gmail.com>
2023-02-02 08:09:59 -0300

    Ejercicio 10BIS Devita Mateo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -6,6 +6,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	// Ejercicio 10
 	let txtSueldo; 
 	let numeroTexto; 
 	let descuento; 
@@ -18,4 +19,21 @@ function mostrarAumento()
 	sueldoDescuento=(txtSueldo-descuento); 
 
 	document.getElementById("txtIdResultado").value = sueldoDescuento; 
+	// Fin Ejercicio 10 
+
+	// Inicio Ejercicio 10BIS
+	//let txtSueldo; 
+	//let descripcion; 
+	//let numeroTexto; 
+	//let descuento; 
+	//let sueldoDescuento; 
+	
+	//txtSueldo=parseInt(document.getElementById("txtIdImporte").value); 
+	//descripcion=prompt("Ingrese Numero de descuento (Solo el entero)"); 
+	//parseInt(descripcion);
+	//descuento=((descripcion/100)*txtSueldo); 
+	//sueldoDescuento=(txtSueldo-descuento); 
+
+	//document.getElementById("txtIdResultado").value = sueldoDescuento; 
+	// Fin Ejercicio 
 }
