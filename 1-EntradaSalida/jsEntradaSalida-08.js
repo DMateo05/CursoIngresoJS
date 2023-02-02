a16cc49b8704d18ed77c1a7772028316e193d25b
DMateo <devitamateo16@gmail.com>
2023-02-02 07:54:38 -0300

    Ejercicio 8BIS Devita Mateo

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,7 +5,8 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
-{
+{	
+	// Ejercicio 8
 	let numeroUno; 
 	let numeroDos; 
 	let numUnoTexto; 
@@ -21,4 +22,24 @@ function SacarResto()
 	mensaje="El resto es " + (numeroUno % numeroDos); 
 
 	alert(mensaje); 
+	// Fin Ejercicio 8 
+
+	// Inicio Ejercicio 8BIS
+	//let numeroUno; 
+	//let numeroDos; 
+	//let resultadoSuma; 
+	//let resultadoPromedio; 
+	//let resto;
+
+	//numeroUno=parseInt(document.getElementById("txtIdNumeroDividendo").value); 
+	//numeroDos=parseInt(document.getElementById("txtIdNumeroDivisor").value); 
+
+	//resultadoSuma=(numeroUno+numeroDos); 
+	//resultadoPromedio=(resultadoSuma/2); 
+	//resto=(numeroUno % numeroDos); 
+
+	//alert("La suma da: " + resultadoSuma); 
+	//alert("El promedio es: " + resultadoPromedio); 
+	//alert("El resto es: " + resto); 
+	// Fin Ejercicio 8BIS
 }
