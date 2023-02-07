46c5c38772392d0655569acfa5b7f32fb2509b08
DMateo <devitamateo16@gmail.com>
2023-02-07 12:39:04 -0300

    Ejercicio 4 Devita Mateo

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,20 @@
+// Devita Mateo 
+// Al ingresar una edad debemos informar si la persona es adolescente, edad entre 13 y 17 años (inclusive) . 
 function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+{	
+	// Declaro Variables
+	let edad = parseInt(document.getElementById("txtIdEdad").value); 
+	let mensaje 
 
+	// Declaro Condicionales 
+	if(edad >= 13 && edad <= 17)
+	{
+		mensaje = "Usted tiene entre 13 y 17 Años";
+	}else
+	{
+		mensaje = "Usted no tiene entre 13 y 17 años"; 
+	} 
+	alert(mensaje); 
 
-}//FIN DE LA FUNCIÓN
+	// Fin Ejercicio
+}
