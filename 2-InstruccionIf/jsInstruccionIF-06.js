d4528c8df0729ad341d95b8c519892bcafdc0f9e
DMateo <devitamateo16@gmail.com>
2023-02-07 12:45:44 -0300

    Ejercicio 6 Devita Mateo

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,23 @@
+// Devita Mateo 
+// Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años). 
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad = parseInt(document.getElementById("txtIdEdad").value); 
+	let mensaje;
+	
+	if(edad >= 18)
+	{
+		mensaje = "Es mayor de Edad"; 
+	}else{
+		if(edad >= 13)
+		{
+			mensaje = "Es Adolescente";
+		}else
+		{
+			mensaje = "Es un Niño";
+		}
+	}
+	alert(mensaje); 
 
-
-
-}//FIN DE LA FUNCIÓN
+	// Fin Ejercicio 
+}	
