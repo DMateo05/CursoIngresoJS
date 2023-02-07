39d454842b3184b5100a7402300868e816f5fda3
DMateo <devitamateo16@gmail.com>
2023-02-07 12:25:24 -0300

    Ejercicio 1 Devita Mateo

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,29 @@
+// Devita Mateo 
+// Ejercicio Al ingresar una edad que sea igual a 15, mostrar el mensaje "niña bonita".
 function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+{ 
+	// Declaro Variables 
+	let mensaje = "Niña Bonita"; 
+	let ageCondicional = 15;
+	let edad = parseInt(document.getElementById("txtIdEdad").value); 
 
-}//FIN DE LA FUNCIÓN
+	// Asigno Condicionales
+	if(edad == ageCondicional)
+	{
+		alert(mensaje);
+	}
+	
+	// Fin Ejercicio 
+
+	/* 
+	- Operadores Aritmeticos 
+	+    -      *              /          % 
+	Mas  Menos  Multiplicar    Dividir    Modulo o Resto
+	> Relacionales Condicionales 
+	<          >         <=               >=              ==      != 
+	Menor de   Mayor de  Menor o Igual    Mayor o Igual   Igual 
+	> Operadores Logicos 
+	&&         ||          ! 
+	y Logica   o Logica    Negacion
+	*/
+}
