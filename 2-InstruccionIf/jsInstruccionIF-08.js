0dd7b0aa5fb20dbbf0f21817a7a79269286d8946
DMateo <devitamateo16@gmail.com>
2023-02-08 10:33:04 -0300

    Ejercicio 8 Devita Mateo

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,25 @@
+// Devita Mateo 
+/* Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'
+*/
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	// Declaro Variables
+	let edad; 
+	let estadoCivil; 
+	let mensaje; 
 
+	// Tomo los Datos 
+	edad = parseInt(document.getElementById("txtIdEdad").value); 
+	estadoCivil = document.getElementById("estadoCivil").value; 
 
-}//FIN DE LA FUNCIÓN
+	// Añado las condiciones 
+	if(edad > 17 && estadoCivil == "Soltero")
+	{
+		// Muestro el Mensaje
+		mensaje = "Es soltero y no es menor"; 
+		alert(mensaje); 
+	} 
+
+	// Fin Ejercicio
+}
