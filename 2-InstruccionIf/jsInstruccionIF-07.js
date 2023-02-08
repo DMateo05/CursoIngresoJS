ccac8320a227822ecf68393f0def473ff8685e3b
DMateo <devitamateo16@gmail.com>
2023-02-08 10:32:24 -0300

    Ejercicio 7 Devita Mateo

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,24 @@
+// Devita Mateo 
+/* Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 
+'Es muy pequeño para NO ser soltero.*/
 function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
-	
+{	
+	// Declaro Variables
+	let edad; 
+	let estadoCivil; 
+	let mensaje; 
 
+	// Tomo Datos
+	edad = parseInt(document.getElementById("txtIdEdad").value); 
+	estadoCivil = document.getElementById("estadoCivil").value; 
 
-}//FIN DE LA FUNCIÓN
+	// Añado una Condicion
+	if(edad < 18 && estadoCivil != "Soltero")
+	{ 
+		// Muestro el Mensaje 
+		mensaje = "Es muy pequeño para NO ser soltero"; 
+		alert(mensaje);
+	} 
+
+	// Fin Ejercicio 
+}
