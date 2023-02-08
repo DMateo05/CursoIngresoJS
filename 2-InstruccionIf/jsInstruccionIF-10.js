f6568cd947eb9fa03318050257725c7e21c46996
DMateo <devitamateo16@gmail.com>
2023-02-08 10:34:27 -0300

    Ejercicio 10 Devita Mateo

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,30 @@
+// Devita Mateo 
+/* Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4 */ 
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	// Declaro Variables
+	let notaNumerica = (Math.floor(Math.random() * (11-1) + 1)); // Del 1 al 10
+	let mensaje; 
 
-}//FIN DE LA FUNCIÓN
+	// Asigno Condiciones
+	if(notaNumerica >= 9)
+	{
+		mensaje = "EXCELENTE: " + notaNumerica;
+	}else
+		{
+			if(notaNumerica >= 4)
+			{
+				mensaje = "APROBÓ: " + notaNumerica;
+			}else
+			{	
+				mensaje = "Vamos, la proxima se Puede: " + notaNumerica;
+			}
+		} 
+	// Muestro el Mensaje
+	alert(mensaje); 
+	
+	// Fin Ejercicio
+}	
