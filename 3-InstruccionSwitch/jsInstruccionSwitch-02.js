4f481e36eddc7a4d859b367abe1e13327059093b
DMateo <devitamateo16@gmail.com>
2023-02-09 13:45:31 -0300

    Ejercicio 2 Devita Mateo

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,34 @@
+/* 
+Devita Mateo 
+al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno. */
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesdelAño; 
+	let mensaje; 
 
+	mesdelAño = document.getElementById("txtIdMes").value; 
 
-
-
-}//FIN DE LA FUNCIÓN
+	switch(mesdelAño)
+	{
+		case "Julio": 
+		case "Agosto":
+			mensaje = "Abrigate que hace Frio."; 
+			break; 
+		case "Septiembre": 
+		case "Octubre": 
+		case "Noviembre": 
+		case "Diciembre": 
+		case "Enero": 
+		case "Febrero": 
+		case "Marzo": 
+			mensaje = "Ya pasamos el frio, ahora calor." 
+			break; 
+		default: 
+			mensaje = "Falta para el invierno" 
+	} 
+	alert(mensaje); 
+}
