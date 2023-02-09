9b2e970aa03af09e8527cedba0e9c90a2089f101
DMateo <devitamateo16@gmail.com>
2023-02-09 13:47:50 -0300

    Ejercicio 4 Devita Mateo

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,32 @@
+/* 
+Devita Mateo 
+al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.*/
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mesDelAño; 
+	let mensaje; 
 
+	mesDelAño = document.getElementById("txtIdMes").value; 
 
-
-}//FIN DE LA FUNCIÓN
+	switch(mesDelAño)
+	{
+		case "Febrero": 
+			mensaje = "Tiene 28 Dias";
+			break; 
+		case "Enero": 
+		case "Marzo": 
+		case "Mayo": 
+		case "Julio": 
+		case "Agosto": 
+		case "Octubre": 
+		case "Diciembre": 
+			mensaje = "Tiene 31 Dias"; 
+			break; 
+		default: 
+			mensaje = "Tiene 30 Dias"; 
+	} 
+	alert(mensaje); 
+}
