527a5fa960fe57dd101630a32ee3a495211ed64a
DMateo <devitamateo16@gmail.com>
2023-02-09 10:18:03 -0300

    TP4 Devita Mateo

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,86 +8,69 @@ C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. */
-function CalcularPrecio () 
+function CalcularPrecio() 
 {
     // txtIdCantidad 
     // Marca 
     // txtIdprecioDescuento 
 
-    // Variables Generales 
-    let basePrecio = 35; 
-    let cantidadLamparas; 
-    let marcaEmpresa; 
-    let resultado; 
+    // Variables 
+    let empresa; 
+    let lamparitas; 
+    let porcentaje; 
+    let precioBase = 35;  
+    let precioVer;
+    let finalUno; 
+    let precioFinal; 
+    let impuesto; 
+    let aumento = 10; 
+    let precioImpuesto; 
 
-    cantidadLamparas = document.getElementById("txtIdCantidad").value; 
-    marcaEmpresa = document.getElementById("Marca").value; 
+    lamparitas = parseInt(document.getElementById("txtIdCantidad").value); 
+    empresa = document.getElementById("Marca").value; 
 
-    if(cantidadLamparas > 5){ 
-        // PUNTO A 
-        aumentoUno = basePrecio*cantidadLamparas; 
-        aumentoUnoBis = aumentoUno*descuentoUno/100; 
-        aumentoTotalA = aumentoUno+aumentoUnoBis; 
-        document.getElementById("txtIdprecioDescuento").value = aumentoTotalA;
-    }else{
-        if(cantidadLamparas == 5 && marcaEmpresa == "ArgentinaLuz"){ 
-            // PUNTO B 
-            aumentoDos = basePrecio*cantidadLamparas; 
-            aumentoDosBis = aumentoDos*descuentoDos/100; 
-            aumentoTotalB = aumentoDos+aumentoDosBis; 
-            document.getElementById("txtIdprecioDescuento").value = aumentoTotalB;
+    // Precio Inicial 
+    precioVer = precioBase*lamparitas 
+
+    // CONDICIONES 
+    if(lamparitas > 5){
+        porcentaje = 50; 
+    }else if(lamparitas == 5){ 
+        if(empresa == "ArgentinaLuz"){
+            porcentaje = 40; 
         }else{
-            if(marcaEmpresa != "ArgentinaLuz"){
-                // PUNTO B 
-                aumentoDos = basePrecio*cantidadLamparas; 
-                aumentoDosBis = aumentoDos*descuentoTres/100; 
-                aumentoTotalB = aumentoDos + aumentoDos; 
-                document.getElementById("txtIdprecioDescuento").value = aumentoTotalB;
-            }
+            porcentaje= 30; 
         }
-    } 
-    if(cantidadLamparas == 4 && (marcaEmpresa == "ArgentinaLuz" || marcaEmpresa == "FelipeLamparas")){
-        // PUNTO C 
-        aumentoTres = basePrecio*cantidadLamparas; 
-        aumentoTresBis = aumentoTres*descuentoCuatro/100; 
-        aumentoTotalC = aumentoTres+aumentoTresBis; 
-        document.getElementById("txtIdprecioDescuento").value = aumentoTotalC; 
-    }else{
-        if(marcaEmpresa != "ArgentinaLuz" || marcaEmpresa != "FelipeLamparas"){
-            // PUNTO C 
-            aumentoTres = basePrecio*cantidadLamparas; 
-            aumentoTresBis = aumentoTres*descuentoCinco/100; 
-            aumentoTotalC = aumentoTres+aumentoTresBis; 
-            document.getElementById("txtIdprecioDescuento").value = aumentoTotalC; 
-        }
-    } 
-
-    if(cantidadLamparas == 3 && marcaEmpresa == "ArgentinaLuz"){ 
-        // PUNTO D
-        aumentoCuatro = basePrecio*cantidadLamparas; 
-        aumentoCuatroBis = aumentoCuatro*descuentoSeis/100; 
-        aumentoTotalD = aumentoCuatro+aumentoCuatroBis; 
-        document.getElementById("txtIdprecioDescuento").value = aumentoTotalD; 
-    }else{
-        if(marcaEmpresa != "ArgentinaLuz" && marcaEmpresa == "FelipeLamparas"){ 
-            // PUNTO D 
-            aumentoCuatro = basePrecio*cantidadLamparas; 
-            aumentoCuatroBis = aumentoCuatro*descuentoSiete/100; 
-            aumentoTotalD = aumentoCuatro+aumentoCuatroBis; 
-            document.getElementById("txtIdprecioDescuento").value = aumentoTotalD; 
+    }else if(lamparitas == 4){
+        if (empresa == "ArgentinaLuz" || empresa == "FelipeLamparas"){
+            porcentaje = 25; 
         }else{
-            if(marcaEmpresa != "FelipeLamparas"){ 
-                // PUNTO D 
-                aumentoCuatro = basePrecio*cantidadLamparas; 
-                aumentoCuatroBis = aumentoCuatro*descuentoOcho/100; 
-                aumentoTotalD = aumentoCuatro+aumentoCuatroBis; 
-                document.getElementById("txtIdprecioDescuento").value = aumentoTotalD; 
-
-            }
+            porcentaje = 20; 
         }
-    } 
+    }else if(lamparitas == 3){
+        if(empresa == "ArgentinaLuz"){
+            porcentaje = 15;
+        }else if(empresa = "FelipeLamparas"){
+            porcentaje = 10; 
+        }else{ 
+            porcentaje = 5; 
+        }
+    }else{
+        porcentaje = 0;
+    }
 
+    // Calculos 
+    finalUno = precioVer*porcentaje/100; 
+    precioFinal = precioVer - finalUno; 
+    document.getElementById("txtIdprecioDescuento").value = precioFinal; 
 
-
-
+    // PUNTO E 
+    if(precioFinal >= 120){
+        impuesto = precioFinal*aumento/100; 
+        precioImpuesto = precioFinal + aumento; 
+    }
+    document.getElementById("txtIdprecioDescuento").value = precioFinal; 
 }
+
+
+
