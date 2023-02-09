c230b70ac6d0d8e5d3f7802a89866b035577ab4f
DMateo <devitamateo16@gmail.com>
2023-02-09 13:57:08 -0300

    Ejercicio 7 Devita Mateo

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,26 @@
+// Devita Mateo 
+// Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste 
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+    let destino; 
+    let mensaje; 
 
-}//FIN DE LA FUNCIÓN
+    destino = document.getElementById("txtIdDestino").value; 
+
+    switch(destino)
+    {
+        case "Bariloche": 
+            mensaje = "Oeste"; 
+            break; 
+        case "Cataratas": 
+            mensaje = "Norte"; 
+            break; 
+        case "Mar del plata": 
+            mensaje = "Este"; 
+            break; 
+        case "Ushuaia": 
+            mensaje = "Sur"; 
+            break; 
+    } 
+    alert(mensaje); 
+}
