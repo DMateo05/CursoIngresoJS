658bce25817d7740bf487ff061ab3267cfcd98ec
DMateo <devitamateo16@gmail.com>
2023-02-09 14:00:57 -0300

    Ejercicio 8 Devita Mateo

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,22 @@
+// Devita Mateo 
+// Al seleccionar un destino informar si hace FRIO o CALOR en ese destino
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino; 
+    let mensaje; 
 
-}//FIN DE LA FUNCIÓN
+    destino = document.getElementById("txtIdDestino").value; 
+
+	switch(destino)
+	{
+		case "Bariloche": 
+		case "Ushuaia": 
+			mensaje = "Hace frio"; 
+			break; 
+		case "Cataratas": 
+		case "Mar del plata": 
+			mensaje = "Hace calor"; 
+			break; 
+	} 
+	alert(mensaje); 
+}
