75a0372f0518fe6051cd170def17c14dc6c103ff
DMateo <devitamateo16@gmail.com>
2023-02-09 13:48:47 -0300

    Ejercicio 5 Devita Mateo

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,11 +1,26 @@
+/* 
+Devita Mateo 
+Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.". 
+*/
 function mostrar()
 {
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
-	
+	let hora
+	let mensaje; 
 
+	hora = parseInt(document.getElementById("txtIdHora").value); 
 
-
-}//FIN DE LA FUNCIÓN
+	switch(hora)
+	{
+		case 7: 
+		case 8: 
+		case 9: 
+		case 10: 
+		case 11: 
+			mensaje = "Es de Mañana" 
+			break; 
+		default: 
+			mensaje = "No es de Mañana"; 	
+	} 
+	alert(mensaje); 
+}
