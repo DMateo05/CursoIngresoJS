ad2d6032bbfbbd847b4ab5894f9788ce949c0fbc
DMateo <devitamateo16@gmail.com>
2023-02-15 08:42:34 -0300

    Ejercicio 4 Devita Mateo

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,9 +1,17 @@
-/*
+/* 
+Devita Mateo
 al presionar el botón 
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
-	
-}//FIN DE LA FUNCIÓN
+	let numeroIngresado;
+	numeroIngresado = prompt("ingrese un número entre 0 y 9."); 
+	numeroIngresado = parseInt(numeroIngresado); 
+
+	while(isNaN(numeroIngresado) || numeroIngresado < 0 || numeroIngresado > 10)
+	{
+		numeroIngresado = prompt("Error, ingrese numero valido"); 
+		numeroIngresado = parseInt(numeroIngresado); 
+	} 
+	document.getElementById("txtIdNumero").value = numeroIngresado; 
+}
