6a1b6ad8ead4e49db6d4c2d48441af51801fcf02
DMateo <devitamateo16@gmail.com>
2023-02-15 08:43:15 -0300

    Ejercicio 5 Devita Mateo

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -1,12 +1,24 @@
-/*
+/* 
+Devita Mateo
 Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
-{
-	var sexoIngresado;
-	sexoIngresado = prompt("ingrese f ó m .");
+{ 
+	let sexoIngresado; 
+	let mensaje; 
 
+	sexoIngresado = prompt('Escriba "F" si es mujer o "M" si es hombre'); 
 
+	while(sexoIngresado != 'F' && sexoIngresado != 'M' && sexoIngresado != "f" && sexoIngresado != "m")
+	{ 
+		sexoIngresado = prompt('Error, Escriba "F" si es mujer o "M" si es hombre'); 
+	} 
 
-	txtIdSexo.value=sexoIngresado;
-}//FIN DE LA FUNCIÓN
+	if(sexoIngresado == "F" || sexoIngresado == "f"){
+		mensaje = "Usted es mujer"; 
+	}else{
+		mensaje = "Usted es Hombre" 
+	} 
+
+	document.getElementById("txtIdSexo").value = mensaje
+}
