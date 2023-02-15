020bccdebda8e4f2a471b2acd52637b98a3dec4d
DMateo <devitamateo16@gmail.com>
2023-02-15 10:18:41 -0300

    Ejercicio 6 Devita Mateo

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,39 @@
+/*
+Devita Mateo 
+Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio. 
+*/
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	// Declaro Variables 
+	let numeroUno; 
+	let contador; 
+	let acumulador; 
+	let promedio; 
+	let i; // Es una variable
 
-	contador=0;
-	acumulador=0;
+	// Inicializo Variables
+	i = 0;  
+	acumulador = 0; 
+	contador = 0; 
+
+	// i se usa para posiciones. Se usa FOR para cantidad fija/finitas. 
+	// for (variable para iterar ; condicion ; aumento/decremento), en este caso no se inicializa la variable i. 
+	// el bucle finito for no existe en Phyton!!. 
+
+	while(i < 5) // While Principal 
+	{
+		i = i + 1; 
+		numeroUno = parseInt(prompt("Ingrese un Numero")); 
+		while(isNaN(numeroUno)) // Validacion 
+		{
+			numeroUno = parseInt(prompt("Error,Ingrese un Numero")); 
+		} 
+		contador = contador + 1; 
+		acumulador = acumulador + numeroIngresado
+	} // Fin While. Promedio dentro del while no se se hace. 
 	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+	promedio = acumulador/contador; 
+
+	document.getElementById("txtIdSuma").value = acumulador; 
+	document.getElementById("txtIdPromedio").value = promedio;  
+}
