7e4bb45afe0900bb185915f31295b442b288c9a1
DMateo <devitamateo16@gmail.com>
2023-02-15 08:40:55 -0300

    Ejercicio 1 Devita Mateo

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -1,7 +1,84 @@
-/*
-al presionar el botón mostrar 10 repeticiones 
-con números ASCENDENTE, desde el 1 al 10.*/
+/* 
+Devita Mateo
+al presionar el botón mostrar 20 repeticiones 
+con números ASCENDENTE, desde el 1 al 10 y 
+A ) Contar los numeros pares 
+B ) Contar los numeros impares 
+C ) El total de la suma de los numeros impares 
+D ) El total de la suma de los numeros pares 
+E ) El mayor de los numeros pares 
+F ) El mayor de los numeros impares 
+G ) El mayor de los numeros  */
 function mostrar()
 {
-	alert('iteración while');
-}//FIN DE LA FUNCIÓN
+	// While genera la linea de codigo muchas veces / Bucle Infinito 
+	// Que es un Bucle? Es una Iteración 
+	// Iteracion = Que se puede generar esa linea de codigo muchas veces 
+	// for = Bucle de Iteracion / Cantidad finita de veces 
+	// do-while = genera automaticamente la primera vez 
+	
+	// Declaro Variable
+	let i; 
+	let contadorPares; 
+	let contadorImpares; 
+	let acumuladorImpares; 
+	let acumuladorPares; 
+	let mayorNumeroPar; 
+	let mayorNumeroImpar; 
+	let mayorNumero; 
+
+	// Inicializo la variable
+	i = 0; 
+	contadorImpares = 0; 
+	contadorPares = 0; 
+	acumuladorImpares = 0; 
+	acumuladorPares = 0; 
+	mayorNumeroImpar = 0; 
+	mayorNumeroPar = 0;  
+	mayorNumero = 0; 
+	
+	//Inicio la Iteracion 
+	while(i < 10) 
+	{ 
+		if(i%2 == 0) // Es un par. 
+		{ 
+			//    0              0 
+			contadorPares = contadorPares + 1;  
+			// Suma numero pares
+			acumuladorPares = acumuladorPares + i; 
+
+			if(i > mayorNumeroPar)
+			{
+				mayorNumeroPar = i; 
+			}
+		}else
+		{
+			contadorImpares = contadorImpares + 1; 
+			// Suma numeros impares
+			acumuladorImpares = acumuladorImpares + i; 
+
+			if(i > mayorNumeroImpar)
+			{
+				mayorNumeroImpar = i; 
+			}
+		}
+		// Modificar la condicion de uno en uno   
+		i = i + 1;  
+		
+		// Mayor Numero 
+		if(i > mayorNumero)
+		{
+			mayorNumero = i; 
+		}
+		// Mostrar Contador 
+		console.log(i);  
+	} // Fin While  
+
+	alert("La cantidad de numeros impares es: " + contadorImpares); 
+	alert("La cantidad de numeros pares es: " + contadorPares); 
+	alert("El total de los numeros impares es: " + acumuladorImpares); 
+	alert("El total de los numeros pares es: " + acumuladorPares); 
+	alert("El mayor numero impar es: " + mayorNumeroImpar); 
+	alert("El mayor de los numeros pares es:" + mayorNumeroPar); 
+	alert("El numero mayor es: " + mayorNumero); 
+}
