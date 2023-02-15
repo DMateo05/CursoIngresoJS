0268a3dd143e15fef5c069b0b33db6df0431eb53
DMateo <devitamateo16@gmail.com>
2023-02-15 08:41:55 -0300

    Ejercicio 3 Devita Mateo

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,8 +1,22 @@
-/*
+/* 
+Devita Mateo
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
-	
-}//FIN DE LA FUNCIÓN
+	// Declaro Variables 
+	let claveIngresada; 
+	let mensaje; 
+
+	// Pido el Dato 
+	claveIngresada = prompt("Ingrese la clave"); 
+
+	// Añado la iteración 
+	while(claveIngresada != "utn750")
+	{
+		claveIngresada = prompt("Error, ingrese la clave correcta"); 
+	} 
+
+	// Muestro Mensaje (Si el usuario coloco bien la clave) 
+	mensaje = "Felicidades, ingreso bien la clave"; 
+	alert(mensaje); 
+}
