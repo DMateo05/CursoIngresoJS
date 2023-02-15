dc756541d4489eedfe19cb48bc9638d64121c3d2
DMateo <devitamateo16@gmail.com>
2023-02-15 11:09:02 -0300

    Ejercicio 9 Devita Mateo

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,19 +3,40 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
+	let banderaDelPrimero;
+	let numeroIngresado;
+	let numeroMaximo;
+	let numeroMinimo;
+	let respuesta; 
+
 	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
+	banderaDelPrimero = true;
+	respuesta="si";
+	while(respuesta == "si") 
+	{ 
+		numeroIngresado = parseInt(prompt("Ingrese un numero")); 
+		//respuesta=prompt("desea continuar? Si/No"); 
+		while(isNaN(numeroIngresado)) // Validacion
+		{
+			numeroIngresado = parseInt(prompt("Error, Ingrese un numero")); 
+		} 
 		
-		respuesta=prompt("desea continuar?");
-	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+		if(banderaDelPrimero == true)
+		{
+			numeroMaximo = numeroIngresado; 
+			numeroMinimo = numeroIngresado; 
+			banderaDelPrimero = false; 
+		}else if(numeroMaximo < numeroIngresado) // numeroIngresado > numeroMaximo
+		{
+			numeroMaximo = numeroIngresado; 
+		}else if(numeroMinimo > numeroIngresado) // numeroIngresado < numeroMinimo
+		{
+			numeroMinimo = numeroIngresado; 
+		}
+
+		respuesta=prompt("desea continuar? si/no"); 
+	} // Fin WHILE 
+
+	document.getElementById("txtIdMaximo").value = numeroMaximo;
+	document.getElementById("txtIdMinimo").value = numeroMinimo;
 }//FIN DE LA FUNCIÓN
