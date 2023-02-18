67fe3155f5e91e7aba13a54900ab0d2b8eef144c
DMateo <devitamateo16@gmail.com>
2023-02-17 22:59:15 -0300

    FOR 7 Devita Mateo

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,23 @@
+// Devita Mateo 
+/* al presionar el botón pedir un número. mostrar los numeros divisores desde el 1 al 
+número ingresado, y mostrar la cantidad de numeros divisores encontrados. */
 function mostrar()
 {
-
-	alert("ok");
-
-
-}//FIN DE LA FUNCIÓN
+		// Declarar Variables 
+		let numero; 
+		let contadorDivisores = 0; 
+	
+		// Pedir un numero 
+		numero = parseInt(prompt("Ingrese un numero")); 
+		
+		for(let i=1; i <= numero;i++)
+		{
+			if(numero%i == 0)
+			{
+				contadorDivisores++; 
+				alert(i);
+			} 
+		} 
+	
+		alert("Cantidad Divisores: " + contadorDivisores);
+}
