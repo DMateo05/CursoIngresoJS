1c2bd8c3d4b80ae5d670616446eac484f1fc33ca
DMateo <devitamateo16@gmail.com>
2023-02-17 22:59:32 -0300

    FOR 8 Devita Mateo

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,32 @@
+// Devita Mateo 
+/*al presionar el botón pedir un número. 
+Informar si el numero es PRIMO o no. */
 function mostrar()
 {
+	// Declarar Variables 
+	let numero; 
+	let contadorDivisores = 0; 
 
+	// Pedir un numero 
+	numero = parseInt(prompt("Ingrese un numero")); 
+	while(isNaN(numero))
+	{
+		numero = parseInt(prompt("Error, Ingrese un numero")); 
+	}
+	
+	for(let i=1; i <= numero;i++)
+	{
+		if(numero%i == 0)
+		{
+			contadorDivisores++; 
+		} 
+	} 
 
-	alert("ok");
-
-}//FIN DE LA FUNCIÓN
+	if(contadorDivisores == 2)
+	{
+		console.log(numero + " es primo")
+	}else
+	{
+		console.log(numero + " no es primo")
+	}
+}
