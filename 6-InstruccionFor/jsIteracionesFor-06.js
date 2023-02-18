35e7a69b647b40e37a2df77ff85848a75ad9d159
DMateo <devitamateo16@gmail.com>
2023-02-17 22:58:55 -0300

    FOR 6 BIS Devita Mateo

--- 6-InstruccionFor/jsIteracionesFor-06.js
+++ 6-InstruccionFor/jsIteracionesFor-06.js
@@ -1,7 +1,26 @@
+// Devita Mateo 
+// al presionar el botón pedir un número. mostrar los numeros pares desde el 1 al número ingresado, y mostrar la cantidad de numeros pares encontrados.
 function mostrar()
 {
-	alert("ok");
+	// Declarar Variables 
+	let numero; 
+	let contadorPares = 0; 
 
+	// Pedir un numero 
+	numero = parseInt(prompt("Ingrese un numero")); 
+	
+	/* Tengo que hacer que una variable 
+	tome los valores desde el 1 hasta el numero ingresado */ 
+	for(let i=1; i <= numero;i++)
+	{
+		// Para cada uno de los valores, analizo la paridad 
+		// si encuentro un par, lo muestro y lo cuento 
+		if(i%2 == 0)
+		{
+			contadorPares++; 
+			alert(i);
+		} 
+	} 
 
-
-}//FIN DE LA FUNCIÓN
+	alert("Cantidad pares: " + contadorPares); 
+}
